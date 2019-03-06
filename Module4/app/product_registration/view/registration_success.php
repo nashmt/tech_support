1cc4e85ed90eefd3351253139cfeab3b1234a788
nashmt <47954716+nashmt@users.noreply.github.com>
2019-03-05 22:03:27 -0500

    Delete registration_success.php

--- Module4/app/product_registration/view/registration_success.php
+++
@@ -1,5 +0,0 @@
-<main>
-    <h1>Success!</h1>
-    <p>You have successfully registered your product (<?php echo $productCode . ') on ' . $getTime->format('M. j, Y \a\t g:i a') ?>.</p>
-    <br>
-</main>
